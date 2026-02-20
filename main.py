23d00895935da802c426210825bfc4b4577bd28a
xsuxrobjon2197 <xsuhrobjon2197@gmail.com>
2026-02-20 15:29:12 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+#1-misol
+def summa(a, b):
+    c = a + b
+    return c
+
+x = summa(2, 3)
+print(x)
+
+#2-misol
+def summa(a, b):
+    c = a * b
+    return c
+
+x = summa(67, 2)
+print(x)
+
+
+#3-misol
+x = input()
+def summa (a, b):
+    c = a*b
+    return c
+
+x1 = summa(1, 2)
+print(x1)
+
+
+#4-misol
+def summa(a, b):
+    c = a + b
+    return c
+
+x = summa(1, 2), max
+print(x)
+
+#5-misol
+def res(a, b):
+    c = input("matiningizni kiriting; ")
+    return c
+
+x = res(67, 67)
+print(x)
